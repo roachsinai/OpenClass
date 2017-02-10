0f5084ff85afa875666aff22c0eec52e93f82b3a
roachsinai <710347541@qq.com>
2017-02-10 21:29:35 +0800

    videos over.

---
+++ CS61A/07.py
@@ -0,0 +1,26 @@
+# Inverse Cascade
+def inverse_cascade(n):
+    grow(n)
+    print(n)
+    shrink(n)
+
+def f_then_g(f, g, n):
+    if n:
+        f(n)
+        g(n)
+
+grow = lambda n: f_then_g(grow, print, n//10)
+shrink = lambda n: f_then_g(print, shrink, n//10)
+
+# count partitions 6由不大于4的正整数组成的所有情况的个数
+def count_partitions(n, m):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        with_m = count_partitions(n-m, m)
+        without_m = count_partitions(n, m-1)
+        return with_m + without_m
---
+++ CS61A/36.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+try:
+    while True:
+        print(' '.join(input()))
+except EOFError:
+    pass
